d0c0c630aed7e7eafa0205b2f7aff67408c234e3
STR1KE6969 <64137481+STR1KE6969@users.noreply.github.com>
2023-08-18 22:53:30 +0300

    fix tooltip

--- src/components/MenuItem.svelte
+++ src/components/MenuItem.svelte
@@ -1,14 +1,22 @@
 <script>
+  import { onMount } from "svelte";
+
   export let tip;
   export let page;
-  let slot;
+  onMount(() => {
+    tippy(`#${tip.toLowerCase().replace(/\s/g, "")}`, {
+      content: tip,
+      theme: "dark",
+      placement: "right",
+      delay: [0, 0],
+      offset: [0, 5],
+    });
+  });
 </script>
 
-<li class="text-2xl static rounded-lg hover:bg-[hsl(var(--bc)/0.1)]">
-  <a
-    class="tooltip tooltip-right absolute h-[40px] w-[62px] text-2xl !bg-transparent"
-    href={page}
-    data-tip={tip}
-  />
-  <slot />
+<li
+  id={tip.toLowerCase().replace(/\s/g, "")}
+  class="text-2xl rounded-lg hover:bg-[hsl(var(--bc)/0.1)]"
+>
+  <a class="!bg-transparent" href={page}> <slot /></a>
 </li>
